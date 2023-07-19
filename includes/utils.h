4b83749164a4e16144bb77e8cc81ba54148f7e64
Marina Vardanyan <50293653+vamarina@users.noreply.github.com>
2023-07-19 19:15:12 +0400

    added function declaration

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: mavardan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 20:31:39 by mavardan          #+#    #+#             */
-/*   Updated: 2023/07/12 20:54:27 by mavardan         ###   ########.fr       */
+/*   Updated: 2023/07/19 18:47:36 by tumolabs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef UTILS_H
@@ -25,5 +25,8 @@ void	vec_normalize(t_vec *vec);
 //sp_cam_utils.c
 t_sphere	*new_sphere(t_vec *center, float diameter, t_rgb *rgb);
 t_camera	*new_cam(t_vec *view_v, t_vec *norm_v, float fov);
+
+//utils.c
+int	is_int_in_range(int val, int left, int right);
 
 #endif
