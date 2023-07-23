b80c07ec7c8509618328c6c6499ea14a67f490cb
Marina Vardanyan <marina.vardanyan@gmail.com>
2023-07-23 18:57:56 +0400

    Added defines.h

---
+++ includes/defines.h
@@ -0,0 +1,14 @@
+#ifndef DEFINES_H
+# define DEFINES_H
+
+# define KEY_X 7
+# define KEY_y 16
+# define KEY_z 6
+# define LEFT 123
+# define RIGHT 124
+# define DOWN 125
+# define UP 126
+# define ESC 53
+# define CLOSE_BTN 17
+
+#endif
