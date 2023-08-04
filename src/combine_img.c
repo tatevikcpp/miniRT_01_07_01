0b3e30d57970c12e5eb932f86753cd019c8c39ee
Marina Vardanyan <marina.vardanyan@gmail.com>
2023-08-04 19:39:51 +0400

    color

--- src/combine_img.c
+++ src/combine_img.c
@@ -15,6 +15,7 @@ void combine_img(t_base *data)
     // int hit_color;
     t_hit *min_hit;
     int color;
+	t_rgb	*tmp_col;
 
     i = 0;
     function(data->rt);
@@ -24,7 +25,6 @@ void combine_img(t_base *data)
         while (j < WIN_HEGHT - 1)
         {
             cam_ray(data->rt, &data->utils->ray, i, j);
-            //printf(" in 2 whiles ");
             // sphere_intersect(&data->utils->ray, data->a_sphere, &data->utils->ray.hit);
             // sphere_intersect(&data->utils->ray, data->a_sphere->next, &data->utils->ray.hit);
             // color = get_closest_sp(data, &data->utils->ray.hit, &data->utils->ray);
@@ -36,25 +36,17 @@ void combine_img(t_base *data)
             {
                 light_ray(data->a_light, &data->utils->ray, min_hit);
                 if (is_in_shadow(data, &data->utils->ray, min_hit->phit))
-                   color = BG_COLOR;
+				{
+					tmp_col = mul_rgb(&min_hit->color, 1);
+					color = rgb_to_int(compute_color_with_amb(tmp_col, data->a_light->amb_col));
+				}
                 else
                     color = rgb_to_int(compute_color(data, &min_hit->color, min_hit));
-                // hit_color = ALBEDO / PI * data->a_light->bright * data->a_light->color * ft
-                // hitColor = hitObject->albedo / M_PI * light->intensity * light->color * std::max(0.f, hitNormal.dotProduct(L));
-                //printf(" before put_pixel ");
-                //printf("----------col in comb img = %d--------\n", rgb_to_int(&min_hit->color));
 		        my_mlx_pixel_put(&data->img_data, i, j, color);
-                //my_mlx_pixel_put(&data->img_data, i, j, 0xffff00);
-                //printf(" after put_pixel ");
                 free_hit(min_hit);
-                // print_hit(&data->utils->ray.hit);
             }
             j++;
         }
         i++;
     }
-            // print_vec(&data->utils->ray.dir);
-    // prinf_vec(&data->utils->ray.dir);
-    // prinf_vec(&data->utils->ray.or);
-    // printf("%f\n", )
 }
